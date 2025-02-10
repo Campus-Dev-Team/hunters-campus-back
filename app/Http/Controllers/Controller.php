bde1049dad03f04778e81cc31cdc84f7019e56b7
Andolon-M <aemendez-l@outlook.com>
2025-02-10 18:11:05 -0500

    feat: :test_tube: probando

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -20,8 +20,7 @@ class Controller extends BaseController {
    * @param http_code $status
    * @return json
    */
-  public function respuesta(bool $success, $data, String $mensaje = '', $status)
-  {
+public function respuesta(bool $success, $data, $status, String $mensaje)  {
     return response()->json([
       'success' => $success,
       'data' => $data,
